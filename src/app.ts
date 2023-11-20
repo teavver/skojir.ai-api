86ea8278e8fa03338b7bfeec777c3774609b6168
teavver <jerzyx11@gmail.com>
2023-11-20 14:03:01 +0100

    app.ts cleanup

--- src/app.ts
+++ src/app.ts
@@ -2,45 +2,53 @@ import { createClient } from "@supabase/supabase-js"
 import { logger, LogType } from "./utils/logger.js"
 import dotenv from "dotenv"
 import express from "express"
-import { exit } from "process"
-import { OpenAI }  from "openai/index.mjs"
+import { OpenAI } from "openai/index.mjs"
 import solverRoute from "./routes/solver.js"
 import statusRoute from "./routes/status.js"
 
 dotenv.config()
 
 const app = express()
-const openAIClient = new OpenAI({apiKey: process.env.OPENAI_KEY})
-
 const MODULE = "main"
 
-function main() {
+function createOpenAIClient() {
+    const apiKey = process.env.OPENAI_KEY
+    if (!apiKey) {
+        logger(MODULE, "Failed to get OpenAI .env key", LogType.ERR)
+        process.exit(1)
+    }
+    logger(MODULE, "Init openAI Client")
+    return new OpenAI({ apiKey })
+}
 
-    logger(MODULE, "Setting up...")
+function createDbClient() {
+    const dbUrl = process.env.SUPABASE_URL
+    const dbKey = process.env.SUPABASE_KEY
+    if (!dbUrl || !dbKey) {
+        logger(MODULE, "Failed to get db .env keys", LogType.ERR)
+        process.exit(1)
+    }
+    logger(MODULE, "Connected to DB")
+    return createClient(dbUrl, dbKey )
+}
+
+function initialize() {
+    logger(MODULE, "Setting up environment...")
     const env = process.env.ENV
     if (!env) {
         logger(MODULE, "No ENV value in .env", LogType.ERR)
-        exit(1)
+        process.exit(1)
     }
 
-    // https://stackoverflow.com/a/40745569/17721532 <- future nginx solver?
-    app.use(express.json({limit: "2.5mb"}))
+    app.use(express.json({ limit: "2.5mb" }))
     app.use("/solve", solverRoute)
     app.use("/", statusRoute)
 
-    logger(MODULE, "Connecting to db...")
-    const supabaseUrl = process.env.SUPABASE_URL
-    const supabaseKey = process.env.SUPABASE_KEY
-    if (!supabaseUrl || !supabaseKey) {
-        logger(MODULE, "Failed to get supabase .env keys", LogType.ERR)
-        exit(1)
-    }
-    
-    const db_client = createClient(supabaseUrl, supabaseKey)
-    logger(MODULE, "Connected to db")
-
+    logger(MODULE, "All set up!")
 }
 
-main()
+const openAIClient = createOpenAIClient()
+const dbClient = createDbClient()
+initialize()
 
-export { app, openAIClient }
+export { app, openAIClient, dbClient }
