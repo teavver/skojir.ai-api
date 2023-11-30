3412d3743e737f6a4b0b7f9330e18d6df1f5dbe7
teavver <jerzyx11@gmail.com>
2023-11-30 03:18:59 +0100

    update password regex

--- src/middlewares/validators/user_services/registerUser.ts
+++ src/middlewares/validators/user_services/registerUser.ts
@@ -48,6 +48,7 @@ const createUserSchema = Joi.object({
         .required(),
         
     password: Joi.string()
-        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
+        .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,64}$"))
+        // 8-64 characters, min 1 lowercase letter, min 1 uppercase letter, min 1 digit, min 1 special character from set {@,$,!,%,*,?,&}
         .required()
 })
