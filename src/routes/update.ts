1bb2fc1de249193634644a32b7b3b31c07998081
teavver <jerzyx11@gmail.com>
2023-12-30 05:53:40 +0100

    hotfix /update endpoint

--- src/routes/update.ts
+++ src/routes/update.ts
@@ -2,5 +2,5 @@ import express from "express"
 import { postUpdate } from "../controllers/update.js"
 
 const router = express.Router()
-router.get("/", postUpdate)
+router.post("/", postUpdate)
 export default router
