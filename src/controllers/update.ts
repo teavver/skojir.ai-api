172a1d923c2200e2a23dacbaa8709717030010e7
teavver <jerzyx11@gmail.com>
2023-12-30 06:02:54 +0100

    fix /update response

--- src/controllers/update.ts
+++ src/controllers/update.ts
@@ -5,5 +5,9 @@ import { logger, LogType } from "../utils/logger.js";
 const MODULE = "controllers :: update"
 
 export async function postUpdate(req: Request, res: Response<ResponseMessage>) {
-    logger(MODULE, req.body, LogType.SERVER)
+    logger(MODULE, JSON.stringify(req.body), LogType.SERVER)
+    return res.status(200).json({
+        state: "success",
+        message: ""
+    })
 }
