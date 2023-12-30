338d63458f08190bdb4564d06ceabaf9118cdf18
teavver <jerzyx11@gmail.com>
2023-12-30 06:22:03 +0100

    test /update webhook

--- src/controllers/update.ts
+++ src/controllers/update.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import { ResponseMessage } from "../types/responses/ResponseMessage.js";
 import { logger, LogType } from "../utils/logger.js";
+import { appendFileSync } from 'fs';
 
 const MODULE = "controllers :: update"
 
 export async function postUpdate(req: Request, res: Response<ResponseMessage>) {
-    logger(MODULE, JSON.stringify(req.body), LogType.SERVER)
+    
+    const bodyStr = JSON.stringify(req.body, null, 2)
+    appendFileSync('update_logs.txt', `${bodyStr}\n`, 'utf8')
+    
+    logger(MODULE, bodyStr, LogType.SERVER)
     return res.status(200).json({
         state: "success",
         message: ""
-    })
+    });
 }
