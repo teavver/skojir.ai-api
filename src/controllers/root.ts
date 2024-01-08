b1e40ba78e5739f7e24718cfe86651433813a2a4
teavver <jerzyx11@gmail.com>
2024-01-08 12:13:33 +0100

    add version info to /root endpoint

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { ResponseMessage } from "../types/responses/ResponseMessage.js";
+import { asyncExec } from "../utils/asyncExec.js";
 
 const MODULE = "controllers :: root"
 
 export async function getRoot(req: Request, res: Response<ResponseMessage>) {
+    const versionId = await asyncExec("git rev-parse --short HEAD", "Failed to get commit id from GH")
     return res.status(200).json({
         state: "success",
-        message: "skojir.ai"       
+        message: `skojir api (${versionId})`
     })
 }
