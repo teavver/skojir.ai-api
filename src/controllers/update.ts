9107d51ecaf1f61f3383ab32a282570ee00b9ad1
teavver <jerzyx11@gmail.com>
2024-01-14 17:38:12 +0100

    add `yarn install` to /update logic

--- src/controllers/update.ts
+++ src/controllers/update.ts
@@ -11,6 +11,7 @@ export async function performUpdate(req: Request, res: Response<ResponseMessage>
     nextTick(async () => {
         logger(MODULE, "Update: Pulling newest updates from GitHub...", LogType.SERVER)
         await asyncExec("git pull origin main", "Failed to pull changes from gh")
+        await asyncExec("yarn", "Failed to update dependencies")
         await asyncExec("yarn build", "Failed to build app")
         await asyncExec("pm2 restart skojir-api", "Failed to restart app after update")
     })
