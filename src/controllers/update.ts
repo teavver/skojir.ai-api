19916bc4001aabe7e5c8487a95f0af04c2d3aaf4
teavver <jerzyx11@gmail.com>
2024-01-14 17:09:44 +0100

    debugging /update again

--- src/controllers/update.ts
+++ src/controllers/update.ts
@@ -2,16 +2,18 @@ import { logger, LogType } from "../utils/logger.js";
 import { asyncExec } from '../utils/asyncExec.js';
 import { Request, Response } from "express";
 import { ResponseMessage } from "../types/responses/ResponseMessage.js";
+import { nextTick } from "process";
 
 const MODULE = "controllers :: update"
 
 export async function performUpdate(req: Request, res: Response<ResponseMessage>) {
-    (async () => {
+
+    nextTick(async () => {
         logger(MODULE, "Update: Pulling newest updates from GitHub...", LogType.SERVER)
         await asyncExec("git pull origin main", "Failed to pull changes from gh")
         await asyncExec("yarn build", "Failed to build app")
         await asyncExec("pm2 restart skojir-api", "Failed to restart app after update")
-    })()
+    })
 
     res.status(200).json({
         state: "success",
